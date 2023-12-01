6f40ef42199a050b13f9c1d89b267f14b533fc80
LuArGo <lucca.ag069@gmail.com>
2023-12-01 11:33:29 -0300

    alt_pesquisa

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
+<?php require("conn_capybd.php");?>
+<?php
+    if(isset($_GET['pesquisa'])){
+        $pesquisa = $_GET['pesquisa'];
+        $query_vaga = "SELECT * FROM tb_pub INNER JOIN tb_tags ON tb_pub.idTag = tb_tags.idTag WHERE tb_pub.ad = 1 AND title LIKE '%$pesquisa%' OR tag LIKE '%$pesquisa%' ORDER BY tb_pub.idPub DESC";
+        $resultado = $conn_capybd->query($query_vaga);
+        echo($resultado);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>index</title>
+  <title>CapyJobs - Seu Site de Eventos</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
     integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -14,7 +23,7 @@
 <body>
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+      <a class="navbar-brand" href="index.html"><img src="images/logo.png"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -48,7 +57,7 @@
         <div class="row">
           <div class="col-xl-6 form-pesquisa" id="banner1">
             <form onsubmit="" class="form-pesquisa-input">
-              <input type="text" placeholder="pesquise um serviço">
+              <input type="text" placeholder="pesquise um serviço" name="pesquisa">
               <button>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
                   viewBox="0 0 16 16">

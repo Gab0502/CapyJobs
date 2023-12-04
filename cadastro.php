56e4d6ca27ad89af4670dd4d3021aad5fce66fc7
LuArGo <lucca.ag069@gmail.com>
2023-12-04 11:01:25 -0300

    alt_php_geral

--- cadastro.php
+++ cadastro.php
@@ -7,39 +7,14 @@
     <title>CapyJobs - Cadastro</title>
     <meta name="description" content="x">
 	<meta name="keywords" content="x">
+    <!-- Padrões do Head -->
     <?php include("_headComum.php")?>
+    <!-- FIM Padrões do Head-->
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="index.old.html"><img src="images/logo.png"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse center" id="navbarNavDropdown">
-              <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Categorias
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Bandas</a>
-                    <a class="dropdown-item" href="#">Entreterimento</a>
-                    <a class="dropdown-item" href="#">Decoração</a>
-                  </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="cadastro.html">CADASTRE-SE</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="login.html">LOGIN</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-    </header>
+    <!-- Header -->
+    <?php include("_header.php")?>
+    <!-- FIM Header -->
     <main>
         <section class="container-fluid">
         
--- index.php
+++ index.php
@@ -15,37 +15,14 @@
   <title>CapyJobs - Seu Site de Eventos</title>
   <meta name="description" content="x">
 	<meta name="keywords" content="x">
+  <!-- Padrões do Head -->
   <?php include("_headComum.php")?>
+  <!-- FIM Padrões do Head-->
 </head>
 <body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand" href="index.php"><img src="images/logo.png"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse center" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Categorias
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Bandas</a>
-              <a class="dropdown-item" href="#">Entreterimento</a>
-              <a class="dropdown-item" href="#">Decoração</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="cadastro.html">CADASTRE-SE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.html">LOGIN</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  <!-- Header -->
+  <?php include("_header.php")?>
+  <!--FIM Header-->
   <main>
     <section class="image-fluid bg-verdeMedio">
       <div class="container-fluid">

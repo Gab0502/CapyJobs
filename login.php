1f0e5fe303ae6017d33b1dae8929632d29c53b7b
LuArGo <lucca.ag069@gmail.com>
2023-12-04 11:37:31 -0300

    alt_general_description

--- login.php
+++ login.php
@@ -5,11 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CapyJobs - Entrar</title>
-    <meta name="description" content="x">
-	<meta name="keywords" content="x">
-    <!-- Padrões do Head -->
-    <?php include("_headComum.php")?>
-    <!-- FIM Padrões do Head-->
+    <meta name="description" content="Entre com seu login">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
     <!-- Header -->

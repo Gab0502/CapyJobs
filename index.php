f74d782997e4e97d5885e98748b39978cba6890d
LuArGo <lucca.ag069@gmail.com>
2023-12-04 09:08:22 -0300

    alt

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php require("conexaoCapybd.php");?>
-<?php
-    if(isset($_GET['pesquisa'])){
-        $pesquisa = $_GET['pesquisa'];
-        $query_vaga = "SELECT * FROM tb_pub INNER JOIN tb_tags ON tb_pub.idTag = tb_tags.idTag WHERE tb_pub.ad = 1 AND title LIKE '%$pesquisa%' OR tag LIKE '%$pesquisa%' ORDER BY tb_pub.idPub DESC";
-        $resultado = $conn_capybd->query($query_vaga);
-        echo($resultado);
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 

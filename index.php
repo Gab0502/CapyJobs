12124260e0bf8ab5dcdd1acc9ceb868e7ebda7b9
LuArGo <lucca.ag069@gmail.com>
2024-01-24 10:23:41 -0300

    alt

--- index.php
+++ index.php
@@ -18,11 +18,7 @@ if (isset($_SESSION['idUser'])) {
   <title>CapyJobs - Seu Site de Eventos</title>
   <meta name="description" content="Site para encontrar servicos para festa, musicas, alimentacao e muito mais. ">
 	<meta name="keywords" content="capyjobs, servicos, empregos, contrato, festa, brinquenos, musicas, banda, animacao, entreterimento, cozinha, comidas, alimentacao">
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="style-login.css">
-  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+  <?php include("_headcom.php")?>
 </head>
 <body>
   <!-- Header -->

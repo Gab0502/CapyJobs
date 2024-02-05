8ac84782cf88ed68e002fe39980650a019f49584
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-05 19:38:12 -0300

    seg e like v1.5

--- feed-temp.php
+++ feed-temp.php
@@ -32,10 +32,10 @@ tb_users.nome,
 tb_users.bio, 
 tb_users.fotoPerfil,
 IFNULL(SUM(tb_likes.idUser = '{$_SESSION['idUser']}'), 0) as userLiked,
-IFNULL(SUM(tb_seg.idSeg1 = '{$_SESSION['idUser']}'), 0) as following
+IFNULL(SUM(tb_seg.idSeg2 = '{$_SESSION['idUser']}'), 0) as following
 FROM tb_pub
 INNER JOIN tb_users ON tb_pub.idUser = tb_users.idUser
-LEFT JOIN tb_likes ON tb_pub.idPub = tb_likes.idPub
+LEFT JOIN tb_likes ON tb_pub.idPub = tb_likes.idPub AND tb_likes.idUser = '{$_SESSION['idUser']}'
 LEFT JOIN tb_seg ON tb_pub.idUser = tb_seg.idSeg1 AND tb_seg.idSeg2 = '{$_SESSION['idUser']}'
 GROUP BY tb_pub.idPub
 ORDER BY tb_pub.dataPub DESC";
@@ -251,6 +251,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             <!-- botão para seguir -->
 
                             <?php
+                            $following = ($row['following'] > 0);
+                            if(isset($row['following'])){
+                                echo("ta funcionando sa porra");
+                            }
                             if ($row['idUser'] == $_SESSION['idUser']) {
                                 // Se o usuário logado é o mesmo que fez a publicação, mostra botões de edição/exclusão
                                 echo '<details>';
@@ -260,7 +264,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 echo '</details>';
                             } else {    
                                 // Caso contrário, mostra botão de seguir/seguindo
-                                echo "<button id='followBtn' onclick=\"follow(" . $row['idUser'] . ")\">" . ($row['following'] ? "Capyseguidor" : "Capyseguir") . "</button>";
+                                if ($following) {
+                                    // Se o usuário estiver seguindo, exiba o botão "Seguindo"
+                                    echo "<button onclick=\"follow({$row['idUser']})\">Capyseguindo</button>";
+                                } else {
+                                    // Se o usuário não estiver seguindo, exiba o botão "Seguir"
+                                    echo "<button onclick=\"follow({$row['idUser']})\">+Capyseguir</button>";
+                                }                          
                             }
                             ?>
                             
@@ -291,8 +301,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='green' class='bi bi-hand-thumbs-up' viewBox='0 0 16 16'>
                                 <path d='M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.040-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725a.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z'/>
                             </svg>
-                            curtir
-                        </button>
+                            <?php 
+                            $liked = ($row['userLiked'] > 0);
+                            if($liked){
+                                echo ("curtido");
+                            }else{
+                                echo ("curtir");
+                            };
+                             ?>                        </button>
                         <button>
                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-reply-fill' viewBox='0 0 16 16'>
                                 <path d='M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z'/>

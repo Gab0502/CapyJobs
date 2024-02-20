8421ba88baec4084007acc644f758291c0f0d2e1
LuArGo <lucca.ag069@gmail.com>
2024-02-20 09:54:02 -0300

    fodase

--- perfil.php
+++ perfil.php
@@ -21,8 +21,7 @@ if(isset($_GET['idUser'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CapyJobs - Perfil de Usuário</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 

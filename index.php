a9ffd71c7addda3e6d97178fe3175a7bd53b1094
LuArGo <lucca.ag069@gmail.com>
2024-02-26 08:37:23 -0300

    bdfds

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $minifeed_row = mysqli_fetch_assoc($minifeed_exe);
                 </svg>
               </button>
               <br><br>
-              <div class="input-buttons">
+              <div style="color: white" class="input-buttons">
                 <h3 style="color: white">Relevantes: </h3>
                 <button action="" class="buttons">Bandas</button>
                 <button class="buttons">Cozinha</button>

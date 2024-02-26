e086ff40a46cb3eda799c98e2c7db80386b1a091
ana <ana@gmail.com>
2024-02-26 11:42:40 -0300

    srfydbv

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $minifeed_row = mysqli_fetch_assoc($minifeed_exe);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CapyJobs - Seu Site de Eventos</title>
-  
+  <link rel="icon" href="images/favicon-16x16.png">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="style-login.css">

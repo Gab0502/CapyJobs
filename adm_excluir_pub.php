7808855a850cc9f59f92fbfabe4c3b1a5cfc4fd1
kauavulgo007 <kauacalisto32@gmail.com>
2024-03-12 11:51:45 -0300

    adm

--- adm_excluir_pub.php
+++ adm_excluir_pub.php
@@ -1,9 +1,10 @@
-<!-- Esta pagina serve apenas para excluir uma determinada publicação! -->
-<?php require('conn_capybd.php');
+<!-- Esta página serve apenas para excluir uma determinada publicação! -->
+<?php 
+require('conn_capybd.php');
 
 if(isset($_GET['idPub'])){
     $idPub = $_GET['idPub'];
-};
+}
 
 // Excluir as imagens da pasta
 $query_rs_img = "SELECT * FROM tb_pub WHERE tb_pub.idPub = $idPub";
@@ -11,36 +12,29 @@ $rs_img = mysqli_query($conn_capybd, $query_rs_img);
 $row_rs_img = mysqli_fetch_assoc($rs_img);
 unlink("images/". $row_rs_img['midia1']);
 
-$query_rs_pub = "DELETE tb_pub, tb_likes 
-FROM tb_pub 
-LEFT JOIN tb_likes ON tb_pub.idPub = tb_likes.idPub
-WHERE tb_pub.idPub = $pub";
+// Excluir a publicação da tabela tb_pub
+$query_delete_pub = "DELETE FROM tb_pub WHERE idPub = $idPub";
+$rs_delete_pub = mysqli_query($conn_capybd, $query_delete_pub);
 
-$rs_pub = mysqli_query($conn_capybd, $query_rs_pub);
+// Excluir os likes associados à publicação
+$query_delete_likes = "DELETE FROM tb_likes WHERE idPub = $idPub";
+$rs_delete_likes = mysqli_query($conn_capybd, $query_delete_likes);
 
-
-if($rs_pub == 1){
-	echo('<script> alert("Registro deletado com sucesso!");
-	window.location.href="adm_pub.php"; </script>');
+if($rs_delete_pub && $rs_delete_likes){
+    echo '<script>alert("Registro deletado com sucesso!"); window.location.href="adm_pub.php";</script>';
 } else{
-	echo("Falha ao deletar registro!");
-};
-
-//header('location:adm-lista.php');
-//exit();
-
+    echo "Falha ao deletar registro!";
+}
 ?>
 <!doctype html>
 <html>
 <head>
-<link rel="manifest" href="pwa/manifest.json">
-
-<meta charset="utf-8">
-<title>adm_excluir_pub</title>
-<link rel="icon" href="images/favicon-16x16.png">
+    <link rel="manifest" href="pwa/manifest.json">
+    <meta charset="utf-8">
+    <title>adm_excluir_pub</title>
+    <link rel="icon" href="images/favicon-16x16.png">
 </head>
 <body>
-<script src="pwa/myscripts.js"></script>
-
+    <script src="pwa/myscripts.js"></script>
 </body>
 </html>
